9cce5f35061d6aba35ccb5357d0243fe810ec3bb
Yassr <c14328571@mydit.ie>
2017-11-02 08:18:35 +0000

    Short Note Summarisation Web Application
    Short Note Summarisation Web Application

---
+++ uploads/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+# Create your views here.
+def home(request):
+    return render(request,'uploads/index.html')
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'uploads/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'uploads/simple_upload.html')
