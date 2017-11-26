f4120b7693258152cefc22ade48742afaf4193a2
Yassr <c14328571@mydit.ie>
2017-11-26 23:25:01 +0000

    Added to Home page
    Added to Home page

--- uploads/views.py
+++ uploads/views.py
@@ -3,8 +3,6 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 import subprocess
 
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-
 
 
 
@@ -21,13 +19,13 @@ def simple_upload(request):
         uploaded_file_url = fs.url(filename)
         if uploaded_file_url.endswith('.mp3'):
             file_extentionA = '.mp3'
-            return render(request, 'uploads/simple_upload.html', {
+            return render(request, 'uploads/home.html', {
             'file_extentionA': file_extentionA, 'uploaded_file_url': uploaded_file_url})
         elif uploaded_file_url.endswith('.mp4'):
             file_extentionV = '.mp4'
-            return render(request, 'uploads/simple_upload.html', {
+            return render(request, 'uploads/home.html', {
             'file_extentionV': file_extentionV, 'uploaded_file_url': uploaded_file_url})
-    return render(request, 'uploads/simple_upload.html')
+    return render(request, 'uploads/home.html')
 
 
 def video_clip(request):
@@ -42,9 +40,8 @@ def video_clip(request):
         #                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         # videoSize = [x for x in result.stdout.readlines() if "Duration" in x]
 
-        # videoduration = ""
 
-        ffmpeg_extract_subclip(uploaded_file_url, '00:00:02', '00:00:04', targetname="/shortnote/media/test.mp4")
+        # ffmpeg_extract_subclip(uploaded_file_url, '00:00:02', '00:00:04', targetname="/shortnote/media/test.mp4")
 
         # subprocess.call(['ffmpeg', '-i', uploaded_file_url], stdout=videoduration)
         #
@@ -54,9 +51,11 @@ def video_clip(request):
         #     if "duration" in info:
         #         print(info)
         #         videoduration = (info)
+        videoduration = subprocess.check_output('ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 '+uploaded_file_url+' -sexagesimal', stdout=videoduration)
 
-
-        return render(request, 'uploads/simple_upload.html')
+        return render(request, 'uploads/home.html',{
+            "video_size": videoduration
+        })
 
 
         # subprocess.call(['ffmpeg', '-i', uploaded_file_url, '-ss', seconds, outputfilename])
